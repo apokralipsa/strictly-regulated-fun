940cc7e6ad9e6998f7f63a87e9aff0d35b8dfa71
Jakub Stawiarski <stawiarskijakub@gmail.com>
2020-07-11 16:59:37 +0200

    Remove compiled files from repo

--- packages/ecs/lib/component.spec.d.ts
+++
@@ -1,5 +0,0 @@
-export interface Position {
-    x: number;
-    y: number;
-}
-export declare function isPosition(input: any): input is Position;
--- packages/ecs/lib/ecs.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=ecs.js.map
